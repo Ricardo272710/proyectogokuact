53952e9c3c64a77d80a94e9a95b0e9b77415fe6c
Ivan <162828351+elias1524@users.noreply.github.com>
2024-08-06 19:00:48 -0400

    Actualizar PRUEBA200.js

--- plugins/PRUEBA200.js
+++ plugins/PRUEBA200.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://telegra.ph/file/53dc136ca4fdf6d852d8b.jpg`)).buffer()
+let img = await (await fetch(`https://telegra.ph/file/c161c058a04477cffede1.mp4`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*BERMUDA*`
+let txt = `*Mincho😻*`
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
 }
 handler.help = ['Mincho']
